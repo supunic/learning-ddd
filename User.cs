56662b99be8a748d99bf19f1e9c3bf3dca13da6c
seiya endo <seiya.endo0830@gmail.com>
2021-01-18 01:12:59 +0900

    add: user

---
+++ User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ddd_value_object
+{
+    class User
+    {
+        public static void Main()
+        {
+            //
+        }
+        public UserId Id { get; set; }
+        public UserName Name { get; set; }
+
+        public User(UserId Id, UserName Name)
+        {
+            if (Id == null) throw new ArgumentNullException(nameof(Id));
+            if (Name == null) throw new ArgumentNullException(nameof(Name));
+
+            this.Id = Id;
+            this.Name = Name;
+        }
+    }
+}
---
+++ UserId.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ddd_value_object
+{
+    class UserId
+    {
+        public static void Main()
+        {
+            //
+        }
+        private readonly string value;
+
+        public UserId(string value)
+        {
+            if (value == null) throw new ArgumentNullException(nameof(value));
+
+            this.value = value;
+        }
+    }
+}
