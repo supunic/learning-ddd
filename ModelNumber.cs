4aaf3e8f9c74b49f0d98cf0dc481b951931f689e
seiya endo <seiya.endo0830@gmail.com>
2021-01-18 01:12:54 +0900

    add: modelNumber

---
+++ ModelNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ddd_value_object
+{
+    class ModelNumber
+    {
+        public static void Main()
+        {
+            //
+        }
+        private readonly string productCode;
+        private readonly string branch;
+        private readonly string lot;
+
+        public ModelNumber(string productCode, string branch, string lot)
+        {
+            if (productCode == null) throw new ArgumentNullException(nameof(productCode));
+            if (branch == null) throw new ArgumentNullException(nameof(branch));
+            if (lot == null) throw new ArgumentNullException(nameof(lot));
+
+            this.productCode = productCode;
+            this.branch = branch;
+            this.lot = lot;
+        }
+
+        public override string ToString()
+        {
+            return productCode + "-" + branch + "-" + lot;
+        }
+    }
+}
